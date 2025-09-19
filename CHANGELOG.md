f62b009b78e9aa450c4cb50b11ffb55ca52e8200
Nadia Tarashkevich <nadia.tarashkevich@jetbrains.com>
2025-09-19 11:34:20 -0700

    Initial commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,5 +3,12 @@
 # ExplandFindUsagesNodes Changelog
 
 ## [Unreleased]
+
+## [0.0.1] - 2025-09-16
+
 ### Added
+
 - Initial scaffold created from [IntelliJ Platform Plugin Template](https://github.com/JetBrains/intellij-platform-plugin-template)
+
+[Unreleased]: https://github.com/nadia-tarashkevich/ExplandFindUsagesNodes/compare/v0.0.1...HEAD
+[0.0.1]: https://github.com/nadia-tarashkevich/ExplandFindUsagesNodes/commits/v0.0.1
--- src/main/kotlin/com/github/nadiatarashkevich/explandfindusagesnodes/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/nadiatarashkevich/explandfindusagesnodes/startup/MyProjectActivity.kt
@@ -1,12 +1,57 @@
 package com.github.nadiatarashkevich.explandfindusagesnodes.startup
 
-import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.wm.ToolWindowId
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener
+import com.intellij.ui.treeStructure.Tree
+import com.intellij.util.ui.UIUtil
+import com.intellij.util.ui.tree.TreeUtil
+import java.awt.Component
+import javax.swing.JComponent
+import javax.swing.JTree
 
 class MyProjectActivity : ProjectActivity {
 
     override suspend fun execute(project: Project) {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        val connection = project.messageBus.connect()
+        connection.subscribe(ToolWindowManagerListener.TOPIC, object : ToolWindowManagerListener {
+            override fun stateChanged(toolWindowManager: ToolWindowManager) {
+                val toolWindow = toolWindowManager.getToolWindow(ToolWindowId.FIND) ?: return
+                if (!toolWindow.isVisible) return
+                // Expand all trees within the Find Usages tool window
+                ApplicationManager.getApplication().invokeLater {
+                    val contents = toolWindow.contentManager.contents
+                    contents.forEach { content ->
+                        expandAllTrees(content.component)
+                    }
+                    logger<MyProjectActivity>().info("Find Usages tool window trees expanded")
+                }
+            }
+        })
+    }
+
+    private fun expandAllTrees(component: Component) {
+        val root = component as? JComponent ?: return
+        // Find all Swing JTree and IntelliJ Tree components and expand them
+        val swingTrees: Collection<JTree> = UIUtil.findComponentsOfType(root, JTree::class.java)
+        swingTrees.forEach { tree ->
+            try {
+                TreeUtil.expandAll(tree)
+            } catch (_: Throwable) {
+                // ignore individual expansion errors
+            }
+        }
+        val ideaTrees: Collection<Tree> = UIUtil.findComponentsOfType(root, Tree::class.java)
+        ideaTrees.forEach { tree ->
+            try {
+                TreeUtil.expandAll(tree)
+            } catch (_: Throwable) {
+                // ignore
+            }
+        }
     }
 }
